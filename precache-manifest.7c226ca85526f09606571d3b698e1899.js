2065083fd5ee987449b347f7b52b2d2f0b4b962b
santistebanc <santistebanc94@gmail.com>
2020-04-27 19:52:43 +0200

    Updates

---
+++ precache-manifest.7c226ca85526f09606571d3b698e1899.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "e7b03cce29449737bfdb",
+    "url": "/chat/static/js/app.d0cd5315.chunk.js"
+  },
+  {
+    "revision": "fe174d965ef201b2a0af",
+    "url": "/chat/static/js/runtime~app.e93f51e4.js"
+  },
+  {
+    "revision": "08649bf7b39da54bbf16",
+    "url": "/chat/static/js/2.b24a9aeb.chunk.js"
+  },
+  {
+    "revision": "48c7ed4e7da9792af288a60d7242d615",
+    "url": "/chat/expo-service-worker.js"
+  },
+  {
+    "revision": "d0c694b562b2208635f250762cd7fc79",
+    "url": "/chat/serve.json"
+  },
+  {
+    "revision": "c4afeaadd41e3c1e32f20dea2656ea32",
+    "url": "/chat/index.html"
+  },
+  {
+    "revision": "d7aa858cc8f919db9520d90d61e1bef6",
+    "url": "/chat/manifest.json"
+  },
+  {
+    "revision": "78c1d32ed5edf54c3d3eb544f11ac93b",
+    "url": "/chat/register-service-worker.js"
+  }
+];
